2d05f99db613a2de2bc74e9e62b9a985c378d0f0
Estefany92 <ashley.huertas@udla.edu.ec>
2025-04-29 23:26:26 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tienda_Huertas_Conlago.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
